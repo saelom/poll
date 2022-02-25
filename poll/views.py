2d6486d473135eaf5a89a54e3307de15e37a942e
saelom <bsc39@naver.com>
2022-02-25 12:49:27 +0900

    poll 사이트

--- poll/views.py
+++ poll/views.py
@@ -1,9 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from poll.models import Question
 
 def index(request):
-    return render(request, 'poll/index.html')
+    question_list = Question.objects.all()   # 전체자료 가져오기
+    return render(request, 'poll/index.html', {'question_list':question_list})
     #return HttpResponse("<h1>안녕~ Django!!</h1>")
+
+def detail(request,pk):
+    question = Question.objects.get(id=pk)   # 자료 1개 가져오기
+    return render(request, 'poll/detail.html', {'question':question})
+
+def vote(request, pk):
+    pass
 
 def cart(request):
     cart = "계란"
